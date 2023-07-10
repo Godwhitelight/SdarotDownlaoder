fa4d79fc4b4a8ead5b67994e6490f1b95c5fbcb3
Godwhitelight <73111474+Godwhitelight@users.noreply.github.com>
2023-07-10 17:16:09 +0300

    feat: login

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import threading
 
 from tasks.sdarot.sdarot_download_task import SdarotDownloadTask
 
-task = SdarotDownloadTask(1, 2, 3, sdarot_creds='abuser159:abuser')
+task = SdarotDownloadTask(1, 2, 3, sdarot_creds='username:password')
 task.start()
